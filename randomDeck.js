040bc136ce8f601c5f33a3d991e2499d08979d08
xander-cernek <xandercernek@gmail.com>
2023-01-13 16:58:42 -0800

    Random deck function v1

--- randomDeck.js
+++ randomDeck.js
@@ -1,13 +1,4 @@
-import * as Discord from "discord.js";
-import { MongoClient } from "mongodb";
-import * as fs from "fs";
-import { randomDeck } from "./randomDeck.js";
-
-const secrets = JSON.parse(fs.readFileSync('secrets.json'));
-const uri = `mongodb+srv://${secrets.mongoUser}:${secrets.mongoPass}@cluster0.mn0nlma.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri);
-
-const cards = [
+cards = [
     "Abomination",
     "Absorbing Man",
     "Adam Warlock",
@@ -205,8 +196,15 @@ const cards = [
     "Zero"
 ]
 
+export function randomDeck() {
+  deck = []
 
+  while (deck.length < 12) {
+    const randomCard = cards[Math.floor(Math.random() * cards.length)]
+    if (!deck.includes(randomCard)) {
+      deck.push(randomCard)
+    }
+  }
 
-const botLogin = secrets.botLogin;
-bot.login(botLogin);
-console.log(randomDeck())
+  return deck
+}
